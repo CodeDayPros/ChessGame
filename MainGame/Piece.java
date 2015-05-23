effb684dbc6fc61fa13b9e8b58e16a7f7b240405
PrahladSrikant <prahladsrikant@yahoo.com>
2015-05-23 12:52:19 -0500

    added setters and getters for position

--- MainGame/Piece.java
+++ MainGame/Piece.java
@@ -10,13 +10,32 @@ public class Piece
 {
     private String name;
     private BufferedImage image;
-    public Piece(String str, BufferedImage pic)
+    private int xPos;
+    private int yPos;
+    
+    public Piece(String str, BufferedImage pic, int x, int y)
     {
         name=str;
         image = pic;
+        xPos=x;
+        yPos=y;
     }
     public String getName()
     {
         return name;
     }
+    public int getX()
+    {
+        return xPos;
+    }
+    public int getY()
+    {
+        return yPos;
+    }
+    public void setPos(int x, int y)
+    {
+        xPos=x;
+        yPos=y;
+    }
+    
 }
