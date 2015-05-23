315621da322ee43cf64732259166a806e791c1bb
PrahladSrikant <prahladsrikant@yahoo.com>
2015-05-23 14:22:30 -0500

    added knight class and pics

---
+++ MainGame/Knight.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Knight here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+public class Knight extends Piece
+{
+     public Knight(int x, int y)
+    {
+        super(x,y);
+    }
+    public BufferedImage getImage()
+    {
+        
+        try {
+         BufferedImage image= ImageIO.read(new File("Knight"));
+         return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public String getName()
+    {
+        return "Knight";
+    }
+}
