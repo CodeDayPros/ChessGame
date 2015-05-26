58ed5f6c1a39ebe8e7517dc4fca1765849a73aba
Sergey Papushin <spapushin@gmail.com>
2015-05-26 17:40:30 -0500

    added main method

---
+++ MainGame/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import java.applet.Applet;
+import java.awt.event.*;
+
+public class Main
+{
+    public static void main (String [] args)
+    {
+        JFrame frame = new JFrame();
+        final Applet applet = new GUI();
+        frame.setSize(418, 486);
+
+        frame.getContentPane().add(applet);
+        frame.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent we) {
+                    applet.stop();
+                    applet.destroy();
+                    System.exit(0);
+                }
+            });
+
+        frame.setVisible(true);
+        applet.init();
+        applet.start();
+    }
+}
